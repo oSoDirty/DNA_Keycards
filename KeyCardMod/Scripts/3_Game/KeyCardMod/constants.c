8e04818b21e25707c5609c6a957ecef08d1f6317
Josh <joshuadamakers@gmail.com>
2021-11-15 22:36:15 -0500

    Delete KeyCardMod directory

--- KeyCardMod/Scripts/3_Game/KeyCardMod/constants.c
+++
@@ -1,5 +0,0 @@
-const protected static string kcm_ProfilePath = "$profile:";
-const protected static string m_ConfigFolderName = "KeyCardConfig";
-const protected static string m_ConfigName = "\KeyCard_";
-const protected static string m_ConfigExtension = "_Config.json";
-	
